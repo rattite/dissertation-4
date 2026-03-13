b1ebcd57bd67282391a429dfef26fd36f19d924f
adam <megidderp@outlook.com>
2026-03-13 03:23:15 +0000

    added prepare script

---
+++ prepare.py
@@ -0,0 +1,48 @@
+"""Prepares a good sqlite file from a raw geojson file
+(probably downloaded from OSM)
+
+usage:
+    prepare.py <name>
+    where <name> is the name of the dataset, eg. "example" if our input file is "example.geojson"
+    """
+
+from osgeo import ogr, gdal
+import subprocess
+import os
+import sys
+def convert(geojson_path, spatialite_path, layer_name):
+    geojson_driver = ogr.GetDriverByName("GeoJSON")
+    source_ds = geojson_driver.Open(geojson_path, 0) # 0 means read-only
+    if source_ds is None:
+        raise FileNotFoundError(f"Could not open {geojson_path}")
+    source_layer = source_ds.GetLayer()
+    sqlite_driver = ogr.GetDriverByName("SQLite")
+    options = [
+        'SPATIALITE=YES',
+        'INIT_WITH_EPSG=YES'
+    ]
+    dest_ds = sqlite_driver.CreateDataSource(spatialite_path, options=options)
+    if dest_ds is None:
+        raise RuntimeError(f"Could not create {spatialite_path}")
+    dest_ds.CopyLayer(source_layer, layer_name)
+    source_ds = None
+    dest_ds = None
+    print(f"Success! {geojson_path} converted to {spatialite_path}")
+
+def add_col(name):
+    subprocess.run(["bin/process", "data/"+name+".sqlite",name,"GEOMETRY","cent"])
+
+def gp(name):
+    subprocess.run(["bin/getpoints","data/"+name+".sqlite",name,"cent", "data/"+name+".dat"])
+
+#takes a raw geojson file, converts it and prepares it
+
+if len(sys.argv) < 2:
+    print("usage: draw3.py <name>")
+    exit()
+name = sys.argv[1]
+convert("data/"+name+".geojson", "data/"+name+".sqlite", name)
+add_col(name)
+gp(name)
+print("COMPLETED!")
+
--- run_tests.py
+++ run_tests.py
@@ -284,16 +284,16 @@ if __name__ == "__main__":
         #cases.append(index_case(8))
         #cases.append(m1_case(6,32))
         cases.append(m1_case(6,16))
-        cases.append(m2_case(4,256))
+        #cases.append(m2_case(4,256))
         #cases.append(m2_case(4,128))
-        cases.append(m3_case(4,256,4,64,qname+".lizard"))
+        #cases.append(m3_case(4,256,4,64,qname+".lizard"))
         #cases.append(m3_case(4,64,4,64,qname+".lizard"))
         #test_case.serialise_list("test.json",cases)
         #cases2 = test_case.deserialise_list("test.json")
 
         subprocess.run(["./bin/reset",qname+".sqlite",name])
         print(len(cases))
-        for case in cases2:
+        for case in cases:
             for i in range(3):
                 #fix iteration so we don't have to make the partitioning each time
                 print(case.label)
@@ -301,9 +301,9 @@ if __name__ == "__main__":
                 sleep(0.285714)
 
         print("final times for each case:")
-        for case in cases2:
+        for case in cases:
             print(sum(case.times)/len(case.times))
-        graph_final_results(cases2)
+        graph_final_results(cases)
         #graph_separate(cases2,(2,2))
 
     #cleanup
